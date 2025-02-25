91f7e31adb76ad878d2e4c6d1718b0904d9dca15
Mahmoud Elmogy <elmogy2016@gmail.com>
2025-02-25 22:23:55 +0200

    _^^_

--- src/Command.cs
+++ src/Command.cs
@@ -90,7 +90,7 @@ namespace codecrafters_shell.src
         {
             Process process = new Process();
             process.StartInfo.FileName = command;
-            process.StartInfo.Arguments = string.Join(" ", arguments.Select(argument => argument.Replace("\"", "\\\"").Replace(" ", "\\")));
+            process.StartInfo.Arguments = string.Join(" ", arguments.Select(argument => argument.Replace("\"", "\\\"").Replace(" ", "\\ ")));
             process.Start();
             process.WaitForExit();
         }
